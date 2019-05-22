77e4af77ff4d759a55341e7dcdc53351eba6a38e
Mathieu Dhondt <laundro@gmail.com>
2019-05-22 14:46:36 +0200

    Added DynamicDocument to all tests

--- tests/test_document.py
+++ tests/test_document.py
@@ -7,7 +7,7 @@ from unittest import TestCase
 
 from bson import ObjectId
 import mongoengine as db
-from mongoengine_goodjson import GoodJSONEncoder, Document, EmbeddedDocument
+from mongoengine_goodjson import GoodJSONEncoder, Document, EmbeddedDocument, DynamicDocument
 from mongoengine_goodjson.document import Helper
 
 try:
@@ -47,6 +47,12 @@ class ToJSONTest(TestCase):
                 db.ReferenceField(SelfReferenceDocument)
             )
 
+        class TestDynamicDocument(DynamicDocument):
+            title = db.StringField()
+            references = db.ListField(
+                db.ReferenceField(SelfReferenceDocument)
+            )           
+            
         self.references = [
             SelfReferenceDocument(
                 pk=ObjectId(), name=("test {}").format(counter)
@@ -57,55 +63,63 @@ class ToJSONTest(TestCase):
                 (index + 1) % len(self.references)
             ]
             srd.to_json = MagicMock(side_effect=srd.to_json)
-        self.model_cls = TestDocument
-        self.model = TestDocument(
-            pk=ObjectId(), title="Test", references=self.references
-        )
-        self.model.to_mongo = MagicMock(
-            return_value={
-                "id": self.model.id,
-                "title": self.model.title,
-                "references": self.references
-            }
-        )
+
+        self.model_classes = [TestDocument, TestDynamicDocument]
+        self.models = [
+            TestDocument(pk=ObjectId(), title="Test", references=self.references),
+            TestDynamicDocument(pk=ObjectId(), title="Test", references=self.references),
+        ]
+        
+        for _model in self.model_classes:
+            _model.to_mongo = MagicMock(
+                return_value={
+                    "id": _model.id,
+                    "title": _model.title,
+                    "references": self.references
+                }
+            )
 
     @patch("json.dumps")
-    def test_document(self, dumps):
-        """self.model.to_json should call encode function."""
-        self.model.to_json()
-        dumps.assert_called_once_with(
-            self.model.to_mongo(True), cls=GoodJSONEncoder
-        )
+    def test_document_types(self, dumps):
+        """.to_json should call encode function."""
+        for _model in self.models:
+            _model.to_json()
+            dumps.assert_called_once_with(
+                _model.to_mongo(True), cls=GoodJSONEncoder
+            )
 
     def test_followreference(self):
         """self.references.to_json should be called 3 times for each."""
-        self.model.to_json(follow_reference=True)
-        for (index, reference) in enumerate(self.references):
-            self.assertEqual(
-                reference.to_json.call_count, 3,
-                ("Reference {} should call to_json 3 times").format(index)
-            )
-            reference.to_json.assert_has_calls([
-                call(
-                    cls=GoodJSONEncoder, follow_reference=True,
-                    use_db_field=True, max_depth=3, current_depth=counter
-                ) for counter in range(1, 4)
-            ], any_order=True)
+               
+        for _model in self.models:
+            _model.to_json(follow_reference=True)
+            for (index, reference) in enumerate(self.references):
+                self.assertEqual(
+                    reference.to_json.call_count, 3,
+                    ("Reference {} should call to_json 3 times").format(index)
+                )
+                reference.to_json.assert_has_calls([
+                    call(
+                        cls=GoodJSONEncoder, follow_reference=True,
+                        use_db_field=True, max_depth=3, current_depth=counter
+                    ) for counter in range(1, 4)
+                ], any_order=True)
 
     def test_followreference_max_15(self):
         """self.references.to_json should be called 15 times for each."""
-        self.model.to_json(follow_reference=True, max_depth=15)
-        for (index, reference) in enumerate(self.references):
-            self.assertEqual(
-                reference.to_json.call_count, 15,
-                ("Reference {} should call to_json 15 times").format(index)
-            )
-            reference.to_json.assert_has_calls([
-                call(
-                    cls=GoodJSONEncoder, follow_reference=True,
-                    use_db_field=True, max_depth=15, current_depth=counter
-                ) for counter in range(1, 16)
-            ], any_order=True)
+        for _model in self.models:        
+            _model.to_json(follow_reference=True, max_depth=15)
+            for (index, reference) in enumerate(self.references):
+                self.assertEqual(
+                    reference.to_json.call_count, 15,
+                    ("Reference {} should call to_json 15 times").format(index)
+                )
+                reference.to_json.assert_has_calls([
+                    call(
+                        cls=GoodJSONEncoder, follow_reference=True,
+                        use_db_field=True, max_depth=15, current_depth=counter
+                    ) for counter in range(1, 16)
+                ], any_order=True)
 
 
 class FromJSONTest(TestCase):
@@ -118,12 +132,17 @@ class FromJSONTest(TestCase):
         class TestDocument(Document):
             title = db.StringField()
 
-        self.model_cls = TestDocument
+        class TestDynamicDocument(DynamicDocument):
+            title = db.StringField()
+            
+        self.model_classes = [TestDocument, TestDynamicDocument]
         self.data = json.dumps({"title": "Test"})
-
+        
     @patch("mongoengine_goodjson.document.generate_object_hook")
     def test_document(self, hook_mock):
-        """Document.from_json should call generate_object_hook."""
+        """(Dynamic)Document.from_json should call generate_object_hook."""
         hook_mock.return_value = lambda x: {"title": "Test"}
-        self.model_cls.from_json(self.data)
-        hook_mock.assert_called_once_with(self.model_cls)
+        for _model_cls in self.model_classes:
+            _model_cls.from_json(self.data)
+            hook_mock.assert_called_once_with(_model_cls)
+
